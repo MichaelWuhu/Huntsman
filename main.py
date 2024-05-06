997fbadc942ec5b42440180aa9b06f2a79dae5ab
Michael <mwu250@gmail.com>
2024-05-05 22:59:08 -0700

    initialize super simple for now

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from dotenv import load_dotenv
 import os
 import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="h!", intents=intents)
+
+# on rdy
+@bot.event
+async def on_ready():
+    guild_count = 0
+
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+        guild_count += 1
+    
+    print("Bot is in " + str(guild_count) + " servers")
+
+
+# test command
+@bot.command()
+async def test(ctx):
+    await ctx.send("test")
+    
+
+# run bot
 load_dotenv()
-discord_bot_key = os.environ.get("DISCORD_BOT_KEY")
+TOKEN = os.environ.get("TOKEN")
+bot.run(TOKEN)
